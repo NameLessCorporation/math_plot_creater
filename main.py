be4d634c42a62964e61f69cf0145a829151b44f2
Danil <danil.present@mail.ru>
2018-10-13 18:37:32 +0800

    Add create window for input

--- main.py
+++ main.py
@@ -14,23 +14,98 @@ from functions.hyperbola import *
 
 v = None
 
-k = -1
-a = 4
-b = 4
+k = None
+a = None
+b = None
+
+
+def input_draw():
+	p(k.get())
+	p(a.get())
+	p(b.get())
 
 
 def creat_plot():
 	global v
 	if v.get() == 1:
-		creat_linear(k, a, b)
+		input_arg()
+		creat_linear(k.get(), a.get(), b.get())
 	elif  v.get() == 2:
-		creat_quadratic(k, a, b)
+		input_arg()
+		creat_quadratic(k.get(), a.get(), b.get())
 	elif v.get() == 3:
-		creat_cubic(k, a, b)
+		input_arg()
+		creat_cubic(k.get(), a.get(), b.get())
 	elif v.get() == 4:
-		creat_hyperbola(k, a, b)
+		input_arg()
+		creat_hyperbola(k.get(), a.get(), b.get())
 	elif v.get() == 5:
-		creat_square(k, a, b)
+		input_arg()
+		creat_square(k.get(), a.get(), b.get())
+
+
+def input_arg():
+	global k
+	global a
+	global b
+	#<--Settings for root
+	root1 = Tk()
+
+	root1.title('PLOT_CREATER')
+
+	root1.resizable(0, 0)
+
+	root1.config(bg = '#1FA7E1')
+	root1.config()
+
+	#<--Elements
+	label1 = Label(root1, text = 'Input k:', bg='#1FA7E1', fg='white')
+
+	k = Entry(root1, width = 20)
+
+	label2 = Label(root1, text = 'Input a:', bg='#1FA7E1', fg='white')
+
+	a = Entry(root1, width = 20)
+
+	label3 = Label(root1, text = 'Input b:', bg='#1FA7E1', fg='white')
+
+	b = Entry(root1, width = 20)
+
+	button = Button(root1, text = 'Watch weather', command = input_draw)
+
+	#<--Configs
+	label1.config(font = ('Arial', 15, 'bold'))
+
+	k.config(font = ('Arial', 15, 'bold'))
+
+	label2.config(font = ('Arial', 15, 'bold'))
+
+	a.config(font = ('Arial', 15, 'bold'))
+
+	label3.config(font = ('Arial', 15, 'bold'))
+
+	b.config(font = ('Arial', 15, 'bold'))
+
+	button.config(font = ('Arial', 15, 'bold'))
+
+	#<--Grids
+	label1.grid(column = 0, row = 1)
+
+	k.grid(column = 0, row = 2)
+
+	label2.grid(column = 0, row = 3)
+
+	a.grid(column = 0, row = 4)
+
+	label3.grid(column = 0, row = 5)
+
+	b.grid(column = 0, row = 6)
+
+	button.grid(column = 0, columnspan = 7)
+
+	#<--Start
+	root1.mainloop()
+
 
 def main():
 	global v

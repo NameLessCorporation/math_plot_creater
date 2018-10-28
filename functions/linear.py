7ceb9a3722ca59925e51b45039246b0f92177361
deissh <36655443+deissh@users.noreply.github.com>
2018-10-28 18:02:48 +0300

    Update linear.py

--- functions/linear.py
+++ functions/linear.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-#<--Import lib
+# <--Import lib
+
 import numpy as np
 from ezprint import *
 from tkinter import *
@@ -7,41 +10,38 @@ import matplotlib.pyplot as plt
 
 
 def creat_linear(k, a, b):
-	x_points = []
-	y_points = []
+    x_points = []
+    y_points = []
 
-	for x in range(-10, 11):
-		x_points.append(x - a)
-		y = k * x + b
-		y_points.append(y)
+    for x in range(-10, 11):
+        x_points.append(x - a)
+        y = k * x + b
+        y_points.append(y)
 
-	func_name = 'y = '
-	if k != 1:
-		func_name = func_name + str(k) + ' * '
-	if a > 0:
-		func_name = func_name + '(x - ' + str(a) + ')'
-	elif a < 0:
-		func_name = func_name + '(x + ' + str(a * -1) + ')'
-	else:
-		func_name = func_name + 'x'
+    func_name = 'y = '
+    if k != 1:
+        func_name = func_name + str(k) + ' * '
+    if a > 0:
+        func_name = func_name + '(x - ' + str(a) + ')'
+    elif a < 0:
+        func_name = func_name + '(x + ' + str(a * -1) + ')'
+    else:
+        func_name = func_name + 'x'
 
+    if b > 0:
+        func_name = func_name + ' + ' + str(b)
+    elif b < 0:
+        func_name = func_name + ' - ' + str(b * -1)
 
-	if b > 0:
-		func_name = func_name +  ' + ' + str(b)
-	elif b < 0:
-		func_name = func_name + ' - ' + str(b * -1)
+    os_x = [min(x_points) - 2, max(x_points) + 2]
+    os_y = [min(y_points) - 2, max(y_points) + 2]
 
+    plt.plot(os_x, [0, 0], '-b', markersize=30)
+    plt.plot([0, 0], os_y, '-b', markersize=30)
 
-	os_x = [min(x_points) - 2, max(x_points) + 2]
-	os_y = [min(y_points) - 2, max(y_points) + 2]
+    plt.plot(x_points, y_points, 'r')
 
-	plt.plot(os_x, [0, 0], '-b', markersize = 30)
-	plt.plot([0, 0], os_y, '-b', markersize = 30)
-
-	plt.plot(x_points, y_points, 'r')
-
-	plt.gca().legend(('x', 'y', 'f(x)'))
-	plt.title(func_name)
-	plt.axis('equal')
-	plt.show()
-
+    plt.gca().legend(('x', 'y', 'f(x)'))
+    plt.title(func_name)
+    plt.axis('equal')
+    plt.show()

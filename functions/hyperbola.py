43e3da2e62e60180c1f6f77c8eddaed66680b3f9
deissh <36655443+deissh@users.noreply.github.com>
2018-10-28 18:02:18 +0300

    Update hyperbola.py

--- functions/hyperbola.py
+++ functions/hyperbola.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-#<--Import lib
+# <--Import lib
+
 import math
 import decimal
 import numpy as np
@@ -9,38 +12,35 @@ import matplotlib.pyplot as plt
 
 
 def creat_hyperbola(k, a, b):
-	x_points = []
-	y_points = []
+    x_points = []
+    y_points = []
 
-	x_points = np.around(np.arange(-10, 10, 0.1), decimals=4)
-	y_points = (k / x_points - a) + b
+    x_points = np.around(np.arange(-10, 10, 0.1), decimals=4)
+    y_points = k / x_points - a + b
 
-	func_name = 'y = '
+    func_name = 'y = '
 
-	if a > 0:
-		func_name = func_name + str(k) + '/(x - ' + str(a) + ')'
-	elif a < 0:
-		func_name = func_name + str(k) + '/(x + ' + str(a * - 1) + ')'
-	else:
-		func_name = func_name + str(k) + '/x'
+    if a > 0:
+        func_name = func_name + str(k) + '/(x - ' + str(a) + ')'
+    elif a < 0:
+        func_name = func_name + str(k) + '/(x + ' + str(a * -1) + ')'
+    else:
+        func_name = func_name + str(k) + '/x'
 
+    if b > 0:
+        func_name = func_name + ' + ' + str(b)
+    elif b < 0:
+        func_name = func_name + ' - ' + str(b * -1)
 
-	if b > 0:
-		func_name = func_name +  ' + ' + str(b)
-	elif b < 0:
-		func_name = func_name + ' - ' + str(b * -1)
+    os_x = [min(x_points) - 5, max(x_points) + 5]
+    os_y = [np.amin(y_points) - 5, np.amax(y_points) + 5]
 
+    plt.plot(os_x, [0, 0], '-b', markersize=30)
+    plt.plot([0, 0], os_y, '-b', markersize=30)
 
-	os_x = [min(x_points) - 5, max(x_points) + 5]
-	os_y = [np.amin(y_points) - 5, np.amax(y_points) + 5]
+    plt.plot(x_points, y_points, 'r')
 
-
-	plt.plot(os_x, [0, 0], '-b', markersize = 30)
-	plt.plot([0, 0], os_y, '-b', markersize = 30)
-
-	plt.plot(x_points, y_points, 'r')
-
-	plt.gca().legend(('x', 'y', 'f(x)'))
-	plt.title(func_name)
-	plt.axis('equal')
-	plt.show()
+    plt.gca().legend(('x', 'y', 'f(x)'))
+    plt.title(func_name)
+    plt.axis('equal')
+    plt.show()

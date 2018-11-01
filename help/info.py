d9e180416c7626f776b75e44090cf9d2ea3f0dfd
Danil <danil.present@mail.ru>
2018-11-01 17:57:24 +0800

    Update info.py and added new file

--- help/info.py
+++ help/info.py
@@ -9,7 +9,7 @@ from tkinter import *
 
 
 def info():
-		#<--Settings for secondFrame
+	#<--Settings for secondFrame
 	fourthFrame = Tk()
 
 	fourthFrame.title('Info')
@@ -20,15 +20,16 @@ def info():
 	fourthFrame.iconbitmap('docs/favicon.ico')
 
 	#<--Elements
-	label2 = Label(fourthFrame, text = 'Linear function - y=k(x-a)+b', bg='#1FA7E1', fg='white')
-	label3 = Label(fourthFrame, text = 'Quadratic function - y=k(x-a)^2+b', bg='#1FA7E1', fg='white')
+	label2 = Label(fourthFrame, text = 'Linear function - y=k*(x-a)+b', bg='#1FA7E1', fg='white')
+	label3 = Label(fourthFrame, text = 'Quadratic function - y=k*(x-a)^2+b', bg='#1FA7E1', fg='white')
 	label4 = Label(fourthFrame, text = 'Square function - y=k*sqrt(x-a)+b', bg='#1FA7E1', fg='white')
-	label5 = Label(fourthFrame, text = 'Cubic function - y=k(x-a)^3+b', bg='#1FA7E1', fg='white')
+	label5 = Label(fourthFrame, text = 'Cubic function - y=k*(x-a)^3+b', bg='#1FA7E1', fg='white')
 	label6 = Label(fourthFrame, text = 'Hyperbola function - y=k/(x-a)+b', bg='#1FA7E1', fg='white')
 	label7 = Label(fourthFrame, text = 'Sine function - y=k*sin(x-a)+b', bg='#1FA7E1', fg='white')
 	label8 = Label(fourthFrame, text = 'Cosine function - y=k*cos(x-a)+b', bg='#1FA7E1', fg='white')
 	label9 = Label(fourthFrame, text = 'QuadraticTrinomial - y=ax^2+bx+c', bg='#1FA7E1', fg='white')
 	label10 = Label(fourthFrame, text = 'Factorial function - y=!x', bg='#1FA7E1', fg='white')
+	label11 = Label(fourthFrame, text = '|Linear| function - y=k*|x-a|+b', bg='#1FA7E1', fg='white')
 
 	#<--Configs
 	label2.config(font = ('Arial', 15, 'bold'))
@@ -40,6 +41,7 @@ def info():
 	label8.config(font = ('Arial', 15, 'bold'))
 	label9.config(font = ('Arial', 15, 'bold'))
 	label10.config(font = ('Arial', 15, 'bold'))
+	label11.config(font = ('Arial', 15, 'bold'))
 
 	#<--Grids
 	label2.grid(column = 0, row = 0)
@@ -51,6 +53,8 @@ def info():
 	label8.grid(column = 0, row = 6)
 	label9.grid(column = 0, row = 7)
 	label10.grid(column = 0, row = 8)
+	label11.grid(column = 0, row = 9)
+
 
 	#<--Start
 	fourthFrame.mainloop()

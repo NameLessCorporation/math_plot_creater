d4a2a80af68c1a79f153bf52ebd4cb98770d0d9e
Dmitriy Stepanov <stdian@mail.ru>
2018-11-01 18:09:24 +0800

    Updated function

--- functions/linearModule.py
+++ functions/linearModule.py
@@ -21,7 +21,10 @@ y_points = []
 
 def create_plot(k, a, b):
 	plt.grid(color='k', linestyle='-', linewidth=0.5)
+
 	# With module: y=k*|(x-a)|+b, then
+	func_name = ''
+
 	if a < 0 and b < 0:
 		func_name = 'y=' + str(k) + '|x-' + str(-a) + '|' + '-' + str(-b)
 	elif a > 0 and b > 0:
@@ -30,8 +33,6 @@ def create_plot(k, a, b):
 		func_name = 'y=' + str(k) + '|x-' + str(-a) + '|' + '+' + str(b)
 	elif a > 0 and b < 0:
 		func_name = 'y=' + str(k) + '|x+' + str(a) + '|' + '-' + str(-b)
-
-
 
 	plt.gcf().canvas.set_window_title(func_name)
 
@@ -54,7 +55,11 @@ def create_linear_with_module(k, a, b, lang):
 	global y_points
 	global x_points
 
-	for i in np.arange(-10.0, 10.0, 0.1):
+	xo = round(-b/k, 3)
+
+	x_list = list(np.arange(a - 5.0, a - 0.005, 0.005)) + [a] + list(np.arange(a + 0.05, a + 5.005, 0.005))
+
+	for i in x_list:
 		x_points.append(i)
 
 	for i in x_points:
